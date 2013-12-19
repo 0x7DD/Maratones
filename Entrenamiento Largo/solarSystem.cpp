94a36b3eef2cf3aabdc3c3f7c96072a566706c85
Juan David <juangilopez@gmail.com>
2013-12-19 18:15:10 -0500

    Update solarSystem.cpp

--- Entrenamiento Largo/solarSystem.cpp
+++ Entrenamiento Largo/solarSystem.cpp
@@ -1,5 +1,5 @@
 # include <bits/stdc++.h>
-# define PI acos(0.0)
+# define pi acos(-1.0)
 using namespace std;
 
 double a1,b1,t1,a2,b2,t,e2;
@@ -16,29 +16,29 @@ const double EPS = 1e-5;
 const double N = 10000.0;
 
 
-struct PT { 
-  double x, y; 
+struct PT {
+  double x, y;
   PT() {}
   PT(double x, double y) : x(x), y(y) {}
-  PT(const PT &p) : x(p.x), y(p.y)    {}
-  PT operator + (const PT &p)  const { return PT(x+p.x, y+p.y); }
-  PT operator - (const PT &p)  const { return PT(x-p.x, y-p.y); }
-  PT operator * (double c)     const { return PT(x*c,   y*c  ); }
-  PT operator / (double c)     const { return PT(x/c,   y/c  ); }
+  PT(const PT &p) : x(p.x), y(p.y) {}
+  PT operator + (const PT &p) const { return PT(x+p.x, y+p.y); }
+  PT operator - (const PT &p) const { return PT(x-p.x, y-p.y); }
+  PT operator * (double c) const { return PT(x*c, y*c ); }
+  PT operator / (double c) const { return PT(x/c, y/c ); }
 };
 
 // rotate a point CCW or CW around the origin
-PT RotateCCW90(PT p)   { return PT(-p.y,p.x); }
-PT RotateCW90(PT p)    { return PT(p.y,-p.x); }
-PT RotateCCW(PT p, double t) { 
-  return PT(p.x*cos(t)-p.y*sin(t), p.x*sin(t)+p.y*cos(t)); 
+PT RotateCCW90(PT p) { return PT(-p.y,p.x); }
+PT RotateCW90(PT p) { return PT(p.y,-p.x); }
+PT RotateCCW(PT p, double t) {
+  return PT(p.x*cos(t)-p.y*sin(t), p.x*sin(t)+p.y*cos(t));
 }
 
-double dot(PT p, PT q)     { return p.x*q.x+p.y*q.y; }
-double dist2(PT p, PT q)   { return dot(p-q,p-q); }
-double cross(PT p, PT q)   { return p.x*q.y-p.y*q.x; }
+double dot(PT p, PT q) { return p.x*q.x+p.y*q.y; }
+double dist2(PT p, PT q) { return dot(p-q,p-q); }
+double cross(PT p, PT q) { return p.x*q.y-p.y*q.x; }
 ostream &operator<<(ostream &os, const PT &p) {
-  os << "(" << p.x << "," << p.y << ")"; 
+  os << "(" << p.x << "," << p.y << ")";
 }
 
 
@@ -48,7 +48,7 @@ double elipseAt(double x){
 }
 
 double polarElipseAt(double angle){
-    return (a2*(1.0 - (e2*e2)))/(1.0 - e2*cos(angle));
+    return (a2*(1.0 - (e2*e2)))/(1.0 + e2*cos(angle));
 }
 
 double myFun(double angle1, double angle2){
@@ -70,20 +70,37 @@ int main(){
         double t2 = t1*sqrt((a2/a1)*(a2/a1)*(a2/a1));
         double c2 = sqrt(a2*a2 - b2*b2);
         e2 = sqrt(1.0 - ((b2*b2)/(a2*a2)));
-        double totalarea = PI*a2*b2;
+        double totalarea = pi*a2*b2;
         double sectionPerDay = totalarea/t2;
         double target = ((t <= t2)? t:fmod(t,t2))*sectionPerDay;//if the time to determine the position of p2 is greater than t2 then the time is t%t2.
         double low = 0.0;
-        double high =4.0*PI;
+        double high =4.0*pi;
         double val = high;
+        //cout<<target<<endl;
         while(true){
             val = (low + ((high - low)/2.0));
             double eval = myFun(0.0,val);
+            //cout<<val<<" "<<target<<" "<<eval<<endl;
             if(fabs(eval - target) < EPS) break;
             if(eval < target)low = val;
             else high = val;
         }
-        
+        if(fabs(val - pi) < EPS){
+                printf("Solar System %d: %.3lf %.3lf\n", cont++, -a2, 0.000);
+                continue;        
+        }
+        if(fabs(val - 2.0*pi) < EPS){
+                printf("Solar System %d: %.3lf %.3lf\n", cont++, a2, 0.000);
+                continue;        
+        }
+        if(fabs(val - (pi/2.0)) < EPS){
+                printf("Solar System %d: %.3lf %.3lf\n", cont++, b2, 0.000);
+                continue;        
+        }
+        if(fabs(val - ((3.0*pi)/2.0)) < EPS){
+                printf("Solar System %d: %.3lf %.3lf\n", cont++, -b2, 0.000);
+                continue;        
+        }
         //computing the solution after finding the angle which subtends the area that i'm looking for
         double A = ((tan(val)*tan(val)) + ((b2*b2)/(a2*a2)));
         double B = 2.0*tan(val)*tan(val)*c2;
@@ -91,6 +108,7 @@ int main(){
         double X1 = (-B + sqrt(B*B - (4.0*A*C)))/(2.0*A);//compunting the possible values of x, must be two.
         double X2 = (-B - sqrt(B*B - (4.0*A*C)))/(2.0*A);
         
+        //if(fabs(val - pi) < EPS)
         //computing the positive values of y
         double Y1 = elipseAt(X1);
         double Y2 = elipseAt(X2);
@@ -100,7 +118,7 @@ int main(){
         
         PT ans;
         
-        if(val > 0.0 && val < PI){
+        if(val > 0.0 && val < pi){
             PT p1 = PT(X1, Y1);
             PT p2 = PT(X2, Y2);
             PT focus = PT(-c2,0.0);
@@ -111,7 +129,7 @@ int main(){
             if(dot(RotateCCW(mio1, val), p1) < EPS) ans = PT(X1, Y1);
             else if(dot(RotateCCW(mio2, val), p2) < EPS) ans = PT(X2, Y2);
         }
-        else if(val > PI && val < 2.0*PI){
+        else if(val > pi && val < 2.0*pi){
             PT p1 = PT(X1, Y11);
             PT p2 = PT(X2, Y22);
             PT focus = PT(-c2,0.0);
